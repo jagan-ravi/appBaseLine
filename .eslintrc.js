854a8426c92dc95cd7f3e6beb16beead3398018e
jagan2022 <102948791+jaganravi-rently@users.noreply.github.com>
2023-07-15 17:48:28 +0530

    lint abd prittier

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'unused-imports'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  rules: {
+    'react/prop-types': 'off', // Optional: Disable prop-types rule if not using PropTypes
+    '@typescript-eslint/no-unused-vars': 'error', // Enforce unused variable errors
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
